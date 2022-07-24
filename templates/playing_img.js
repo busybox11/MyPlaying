946f3fcefd328cc96323234770fc8f10c7779297
busybox <busybox11th@gmail.com>
2022-07-24 15:42:57 +0200

    AGAIN

--- templates/playing_img.js
+++ templates/playing_img.js
@@ -19,7 +19,7 @@ module.exports = function(playingObj, height=250, width=600) {
             replaced = replaced.replaceAll('{TRACK_URL}', playingObj.meta.url)
             replaced = replaced.replaceAll('{SPOTIFY_PROFILE_URL}', process.env.SPOTIFY_PROFILE_URL || 'https://open.spotify.com/')
 
-            replaced = replaced.replaceAll('{PLAYING_ICON}', (playingObj.progress.playing) ? '<img src="https://open.spotifycdn.com/cdn/images/equaliser-animated-green.f93a2ef4.gif" height="16">' : '')
+            replaced = replaced.replaceAll('{PLAYING_ICON}', (playingObj.progress.playing) ? '<img src="https://open.spotifycdn.com/cdn/images/equaliser-animated-green.f93a2ef4.gif" height="16" />' : '')
             replaced = replaced.replaceAll('{PLAYING_TEXT}', (playingObj.progress.playing) ? 'Now playing on Spotify' : 'Last played song')
 
             resolve(replaced)
